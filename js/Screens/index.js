b6b05436af8b7a43aac3b73d5ef20af65909c046
Aksels Ledins <aksels.ledins@gmail.com>
2018-01-08 22:16:02 +0100

    (js | Screens) {Home,Settings,Statistics,Workout}

---
+++ js/Screens/index.js
@@ -0,0 +1,5 @@
+
+export { default as HomeScreen } from './Home';
+export { default as SettingsScreen } from './Settings';
+export { default as StatisticsScreen } from './Statistics';
+export { default as WorkoutScreen } from './Workout';
