15746d49a57800754eace302d56ba9bda2ed7ffc
Aksels Ledins <aksels.ledins@gmail.com>
2018-01-08 22:16:20 +0100

    (js | App.js) ultra basic navigation

--- js/App.js
+++ js/App.js
@@ -1,80 +1,78 @@
 /* @flow */
 
 import React from 'react';
-import { Constants, ScreenOrientation } from 'expo';
-
-ScreenOrientation.allow(ScreenOrientation.Orientation.ALL);
+import { ScreenOrientation } from 'expo';
 
 import {
   Platform,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  StatusBar,
-  View,
 } from 'react-native';
-import { SafeAreaView, StackNavigator } from 'react-navigation';
 
-//import Banner from './Banner';
+import { TabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import TabAnimations from './TabAnimations';
+/* screens */
+import {
+  HomeScreen,
+  SettingsScreen,
+  StatisticsScreen,
+  WorkoutScreen,
+} from './Screens';
 
-const ExampleRoutes = {
-  TabAnimations: {
-    name: 'Animated Tabs Example',
-    description: 'Tab transitions have custom animations',
-    screen: TabAnimations,
-  },
-};
+ScreenOrientation.allow(ScreenOrientation.Orientation.ALL);
 
-class MainScreen extends React.Component<*> {
-  render() {
-    const { navigation } = this.props;
-
-    return (
-      <View style={{ flex: 1 }}>
-        <ScrollView style={{ flex: 1 }}>
-          {/*<Banner />*/}
-          {Object.keys(ExampleRoutes).map((routeName: string) => (
-            <TouchableOpacity
-              key={routeName}
-              onPress={() => {
-                const { path, params, screen } = ExampleRoutes[routeName];
-                const { router } = screen;
-                const action =
-                  path && router.getActionForPathAndParams(path, params);
-                navigation.navigate(routeName, {}, action);
-              }}
-            >
-              <SafeAreaView
-                style={styles.itemContainer}
-                forceInset={{ vertical: 'never' }}
-              >
-                <View style={styles.item}>
-                  <Text style={styles.title}>
-                    {ExampleRoutes[routeName].name}
-                  </Text>
-                  <Text style={styles.description}>
-                    {ExampleRoutes[routeName].description}
-                  </Text>
-                </View>
-              </SafeAreaView>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-        <StatusBar barStyle="light-content" />
-        <View style={styles.statusBarUnderlay} />
-      </View>
-    );
-  }
-}
-
-const AppNavigator = StackNavigator(
+const AppNavigator = TabNavigator(
   {
-    ...ExampleRoutes,
     Index: {
-      screen: MainScreen,
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={focused ? 'ios-home' : 'ios-home-outline'}
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
+    },
+    Workouts: {
+      screen: WorkoutScreen,
+      navigationOptions: {
+        tabBarLabel: 'Workouts',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={focused ? 'ios-heart' : 'ios-heart-outline'}
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
+    },
+    Statistics: {
+      screen: StatisticsScreen,
+      navigationOptions: {
+        tabBarLabel: 'Stats',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={focused ? 'ios-analytics' : 'ios-analytics-outline'}
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        tabBarLabel: 'Settings',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={focused ? 'ios-settings' : 'ios-settings-outline'}
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
     },
   },
   {
@@ -86,43 +84,7 @@ const AppNavigator = StackNavigator(
    * which conflicts with the drawer example gesture
    */
     mode: Platform.OS === 'ios' ? 'modal' : 'card',
-  }
+  },
 );
 
 export default () => <AppNavigator />;
-
-const styles = StyleSheet.create({
-  item: {
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-  },
-  itemContainer: {
-    backgroundColor: '#fff',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#ddd',
-  },
-  image: {
-    width: 120,
-    height: 120,
-    alignSelf: 'center',
-    marginBottom: 20,
-    resizeMode: 'contain',
-  },
-  statusBarUnderlay: {
-    backgroundColor: '#673ab7',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: Constants.statusBarHeight,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#444',
-  },
-  description: {
-    fontSize: 13,
-    color: '#999',
-  },
-});
