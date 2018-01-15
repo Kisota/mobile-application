5076adb4280dcdb74c6c173cd98645f26540b9fd
Aksels Ledins <aksels.ledins@gmail.com>
2018-01-15 20:54:30 +0100

    (app.js) sync redux store with local storage

--- js/App.js
+++ js/App.js
@@ -4,16 +4,53 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-
+import { Text, AppState, AsyncStorage, View } from 'react-native';
 import { Root } from './Routes';
 
 const store = configureStore({});
 
 // eslint-disable-next-line
 class App extends React.Component {
+
+  state = {
+    isStoreLoading: false,
+    store,
+  }
+
+  componentWillMount() {
+    const self = this;
+
+    AppState.addEventListener('change', this.handleAppStateChange.bind(this));
+    this.setState({ isStoreLoading: true });
+    AsyncStorage.getItem('completeStore').then((value) => {
+      if (value && value.length) {
+        const initialStore = JSON.parse(value);
+        self.setState({ store: configureStore(initialStore) });
+      } else {
+        self.setState({ store: configureStore({}) });
+      }
+      self.setState({ isStoreLoading: false });
+    }).catch(() => {
+      self.setState({ store });
+      self.setState({ isStoreLoading: false });
+    });
+  }
+
+  handleAppStateChange() {
+    const storingValue = JSON.stringify(this.state.store.getState());
+    AsyncStorage.setItem('completeStore', storingValue);
+  }
+
   render() {
+    if (this.state.isStoreLoading) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Text>Loading Store ...</Text>
+        </View>
+      );
+    }
     return (
-      <Provider store={store}>
+      <Provider store={this.state.store}>
         <Root />
       </Provider>
     );
