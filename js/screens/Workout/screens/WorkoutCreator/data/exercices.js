e737952d313da7d3c94fdcdd98c006e78a0e1192
Aksels Ledins <aksels.ledins@gmail.com>
2018-01-31 18:11:21 +0100

    (screens) Workout|Creator + small data set

---
+++ js/screens/Workout/screens/WorkoutCreator/data/exercices.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    name: 'Dumbbell Bench Press',
+    trains: ['Chest'],
+    type: 'Dumbbell',
+  },
+  {
+    name: 'Pushups',
+    trains: ['Body', 'Chest'],
+    type: 'Only',
+  },
+  {
+    name: 'Dumbbell Flyes',
+    trains: ['Chest'],
+    type: 'Dumbbell',
+  },
+  {
+    name: 'Incline Dumbbell Press',
+    trains: ['Chest'],
+    type: 'Dumbbell',
+  },
+  {
+    name: 'Low Cable Crossover',
+    trains: ['Chest'],
+    type: 'Cable',
+  },
+  {
+    name: 'Decline Dumbbell Flyes',
+    trains: ['Chest'],
+    type: 'Dumbbell',
+  },
+  {
+    name: 'Dips - Chest Version',
+    trains: ['Chest'],
+    type: 'Other',
+  },
+  {
+    name: 'Barbell Bench Press - Medium Grip',
+    trains: ['Chest'],
+    type: 'Barbell',
+  },
+  {
+    name: 'Bodyweight Flyes',
+    trains: ['Chest'],
+    type: 'Curl E-Z Bar',
+  },
+  {
+    name: 'Incline Cable Flye',
+    trains: ['Chest'],
+    type: 'Cable',
+  },
+  {
+    name: 'Wide-Grip Decline Barbell Bench Press',
+    trains: ['Chest'],
+    type: 'Barbell',
+  },
+  {
+    name: 'Incline Dumbbell Press Reverse-Grip',
+    trains: ['Chest'],
+    type: 'Dumbbell',
+  },
+  {
+    name: 'Wide-Grip Barbell Bench Press',
+    trains: ['Chest'],
+    type: 'Barbell',
+  },
+  {
+    name: 'Decline Barbell Bench Press',
+    trains: ['Chest'],
+    type: 'Barbell',
+  },
+  {
+    name: 'Incline Push-Up',
+    trains: ['Body', 'Chest'],
+    type: 'Only',
+  },
+  {
+    name: 'Incline Dumbbell Flyes',
+    trains: ['Chest'],
+    type: 'Dumbbell',
+  },
+  {
+    name: 'Cable Crossover',
+    trains: ['Chest'],
+    type: 'Cable',
+  },
+  {
+    name: 'Neck Press',
+    trains: ['Chest'],
+    type: 'Barbell',
+  },
+];
